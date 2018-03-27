4f015d9b2bbc40f3a529e6b0751163fa8e323536
Dan Reuman <reuman@ku.edu>
2018-03-26 21:27:06 -0500

    finished find_fixed

--- find_fixed.R
+++ find_fixed.R
@@ -12,5 +12,16 @@
 #
 find_fixed<-function(ms,seq)
 {
+  ncms<-nchar(ms)
   
+  res<-c()
+  for (counter in 1:(nchar(seq)-ncms+1))
+  {
+    if (ms==substr(seq,counter,counter+ncms-1))
+    {
+      res<-c(res,counter)
+    }
+  }
+  
+  return(res)
 }
